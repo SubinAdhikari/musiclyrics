f50e7633273b85cf8f9ce9bfdf65bbe74277d26f
SubinAdhikari <subinadhikari61@gmail.com>
2020-07-29 18:17:19 +0545

    Updates

---
+++ precache-manifest.3dabe3f18a1d1113bf4fb3e13f4975a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0bd56e5f3c1df9684db027bcaad100",
+    "url": "/musiclyrics/index.html"
+  },
+  {
+    "revision": "e5867f877ebf90e24360",
+    "url": "/musiclyrics/static/css/main.c205b153.chunk.css"
+  },
+  {
+    "revision": "69928a24a2f8870da8f9",
+    "url": "/musiclyrics/static/js/2.a2fd66d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/musiclyrics/static/js/2.a2fd66d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5867f877ebf90e24360",
+    "url": "/musiclyrics/static/js/main.dee5817d.chunk.js"
+  },
+  {
+    "revision": "93d30e00ff506de887cb",
+    "url": "/musiclyrics/static/js/runtime-main.55cbcbe9.js"
+  }
+]);
